903482634d2b47bb9fd99dc4ecb890e419104a6a
Herocode <thaiduong61130@gmail.com>
2021-09-27 09:14:07 +0700

    ok

---
+++ reactjsmaterialui/src/MyContent.js
@@ -0,0 +1,12 @@
+import react from "react";
+import './MyContent.css'
+class MyContent extends react.Component {
+  render() {
+    return (
+    <div className="content">
+      Xin chào
+    </div>);
+  }
+}
+
+export default MyContent;
